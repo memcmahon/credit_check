86d92bd538306f4f5d9876822b6db980f748771a
memcmahon <mcmahon.meganelizabeth@gmail.com>
2017-11-30 18:09:22 -0700

    Updates Credit Check

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,31 +1,75 @@
-card_number = "4929735477250543"
+def numbers_doubled(number)
+  doubled_card_numbers = []
+  card_numbers = number.reverse.split("")
+  card_numbers.each_with_index do |number, index|
+    if index.odd?
+      double = number.to_i * 2
+      doubled_card_numbers << double
+    else
+      doubled_card_numbers << number.to_i
+    end
+  end
+  doubled_card_numbers
+end
 
-valid = false
-
-# Your Luhn Algorithm Here
-
-#Seperate the digits
-card_numbers = card_number.split("")
-
-#2x every other digit from the right
-doubled_card_numbers = []
-
-card_numbers.each_with_index do |number, index|
-  if index.odd?
-    double = number.to_i * 2
-    doubled_card_numbers << double
-  else
-    doubled_card_numbers << number.to_i
+def digit_inspect_and_sum(number)
+  number.map do |digit|
+    if digit > 9
+      digit - 9
+    else
+      digit
+    end
   end
 end
 
-puts doubled_card_numbers.inspect
+def sum_digits(number)
+  sum = 0
+  number.map do |digit|
+    sum += digit
+  end
+  sum % 10 == 0
+end
 
-#summed digits over 10
+card_number = "4929735477250543"
 
-#results summed and modulo 10 = 0?
+p sum_digits(digit_inspect_and_sum(numbers_doubled(card_number)))
 
 
-# Output
-# If it is valid, print "The number is valid!"
-# If it is invalid, print "The number is invalid!"
+#
+# card_numbers = card_number.reverse.split("")
+#
+# doubled_card_numbers = []
+#
+# card_numbers.each_with_index do |number, index|
+#   if index.odd?
+#     double = number.to_i * 2
+#     doubled_card_numbers << double
+#   else
+#     doubled_card_numbers << number.to_i
+#   end
+# end
+#
+# puts doubled_card_numbers.inspect
+#
+# new_array = doubled_card_numbers.map do |double|
+#   if double > 9
+#     double - 9
+#   else
+#     double
+#   end
+# end
+#
+# puts new_array.inspect
+#
+# sum = 0
+# sum_array = new_array.map do |digit|
+#   sum += digit
+# end
+#
+# puts sum
+#
+# if sum % 10 == 0
+#   puts "The number is valid!"
+# else
+#   puts "The number is invalid!"
+# end
