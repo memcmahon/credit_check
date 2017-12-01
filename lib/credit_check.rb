78cd81b96d671e248b1e52b0046fae7cbfa4023d
memcmahon <mcmahon.meganelizabeth@gmail.com>
2017-11-30 18:18:00 -0700

    Update Credit Check
    update the output

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -30,46 +30,11 @@ def sum_digits(number)
   sum % 10 == 0
 end
 
-card_number = "4929735477250543"
 
-p sum_digits(digit_inspect_and_sum(numbers_doubled(card_number)))
+card_number = "4024007136512380"
 
-
-#
-# card_numbers = card_number.reverse.split("")
-#
-# doubled_card_numbers = []
-#
-# card_numbers.each_with_index do |number, index|
-#   if index.odd?
-#     double = number.to_i * 2
-#     doubled_card_numbers << double
-#   else
-#     doubled_card_numbers << number.to_i
-#   end
-# end
-#
-# puts doubled_card_numbers.inspect
-#
-# new_array = doubled_card_numbers.map do |double|
-#   if double > 9
-#     double - 9
-#   else
-#     double
-#   end
-# end
-#
-# puts new_array.inspect
-#
-# sum = 0
-# sum_array = new_array.map do |digit|
-#   sum += digit
-# end
-#
-# puts sum
-#
-# if sum % 10 == 0
-#   puts "The number is valid!"
-# else
-#   puts "The number is invalid!"
-# end
+if sum_digits(digit_inspect_and_sum(numbers_doubled(card_number)))
+  puts "Number is valid!"
+else
+  puts "Number is invalid!"
+end
